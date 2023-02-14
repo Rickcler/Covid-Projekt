28eb668ed2ee68b811dbf94edab75d8db01488a6
Rickcler <117739535+Rickcler@users.noreply.github.com>
2023-02-14 13:27:59 +0100

    Adding Some Analysis
    Further Analysis was added, including
    Income Data and Ranking Data

--- Covid Data Exploration.sql
+++ Covid Data Exploration.sql
@@ -37,11 +37,14 @@
 
 
 
---------------------------------Worldwide---------------------------------
+--------------------------------Countries----------------------------------
+
 ---Creating a temp-table with the current total values (total_cases, total_deaths etc.)  
 --DROP TABLE IF EXISTS #CountriesTotals
---CREATE TABLE #CountriesTotals (Country NVARCHAR(50), Continent NVARCHAR(50), Population BIGINT, total_cases BIGINT, total_deaths INT, total_cases_pm FLOAT, total_deaths_pm FLOAT)
+--CREATE TABLE #CountriesTotals (Country NVARCHAR(50), Continent NVARCHAR(50), Population BIGINT, total_cases FLOAT, total_deaths FLOAT, total_cases_pm FLOAT, total_deaths_pm FLOAT)
 
+---- Here I am Using the MAX Function for any of those variables, because, at least in principle, this should return the most recent value (as it wouldn't make sense
+---- for those values to go done again. Additionally, I add Continent IS NOT NULL to exclude all the non Country locations.
 --INSERT INTO #CountriesTotals
 --SELECT location Country, Continent, MAX(Population) Population, MAX(total_cases) total_cases, MAX(total_deaths) total_deaths,
 --	   MAX(total_cases_per_million) total_cases_pm, MAX(total_deaths) total_deaths_pm
@@ -59,25 +62,28 @@
 --FROM #CountriesTotals
 --ORDER BY total_deaths DESC
 
----- Comparing positions different countries have in total cases, total deaths, 
 
-
----- Total Death by total cases ratio by country
---SELECT location, MAX(total_deaths)/ MAX(total_cases) Ratio
---FROM CovidDeaths
---GROUP BY location
---ORDER BY Ratio DESC
-
---- Death Rate by continent
-
-
---SELECT continent, ROUND((SUM(total_deaths)/ SUM(total_cases) * 100), 2) Ratio
+---- Table for Total Death by total cases ratio by country in Percent
+---- According to the table, North Korea has a Death Rate of 600% which can only be explained by wrong Data. Hence, it's been excluded.
+--SELECT Country, Continent, CONCAT(ROUND((total_deaths/ total_cases *100), 2), '%') Ratio
 --FROM #CountriesTotals
---WHERE continent IS NOT NULL
---GROUP BY continent
---ORDER BY SUM(total_deaths)/ SUM(total_cases) DESC
+--WHERE Country <> 'North Korea'
+--ORDER BY ROUND((total_deaths/ total_cases), 2) DESC
 
----- Finding the Country for each Month since the onset of Covid which had the highest share of infections
+
+---- Comparing the relative positions different countries have in total cases, total deathsa and death rate
+--SELECT Country, Continent, ROW_NUMBER() OVER (ORDER BY  total_cases DESC) Position_Cases, ROW_NUMBER() OVER (ORDER BY  total_deaths DESC) Position_Deaths, 
+--	   ROW_NUMBER() OVER (ORDER BY total_deaths/ total_cases DESC) Position_Ratio
+--FROM #CountriesTotals
+--WHERE total_cases IS NOT NULL OR total_deaths IS NOT NULL AND Country <> 'North Korea'
+--ORDER BY  ROW_NUMBER() OVER (ORDER BY  total_cases DESC)
+
+/* It becomes quite clear that the Position in the Cases-Ranking and the position in the Deaths-Ranking are only loosely related, with especially European
+   countries having comparativelly low ranks. One possible explanation would be that the actions different countries took indeed had an impact on the Amount of Deaths.
+   The relation to the Position Ratio seems to be non-existent with smaller Asian, African and South American countries being on top here*/
+
+
+---- Finding the Country for each Month since the onset of Covid which had the highest share of new infections
 --WITH ByCountry AS
 --	(SELECT Year(Date) Year, Month(Date) Month, Location, SUM(new_cases) NewCases, SUM(new_deaths) NewDeaths
 --	 FROM CovidDeaths
@@ -116,9 +122,54 @@
 --ORDER BY year, month
 -- Returns much more realistic Results for most Months. However some still seem quite odd with Values over 100%
 
---Romania Has a 113% death rate in June 2021:
+--Romania Has a 113% death rate in June 2021, let's examine this in more Detail:
 --SELECT date, location, new_deaths, new_cases, new_deaths/new_cases * 100 Ratio 
 --FROM CovidDeaths
 --WHERE Year(date) = 2021 AND Month(date) = 6 AND location = 'Romania'
+
 -- In the later part of the month the ratio is far above 100% which probably can only be explained by some errors in the generation of the data
--- They probably had cases where people came in just to die and where not anymore counted as New Cases
+-- They may have had cases where people came in just to die and where not anymore counted as New Cases
+
+
+----------------------------------Continents------------------------------------
+--- Death Rate by continent (still using the same temp table)
+
+--SELECT continent, CONCAT(ROUND((SUM(total_deaths)/ SUM(total_cases) * 100), 2), '%') Ratio
+--FROM #CountriesTotals
+--WHERE continent IS NOT NULL
+--GROUP BY continent
+--ORDER BY SUM(total_deaths)/ SUM(total_cases) DESC
+
+----------------------------------Non Country Locations--------------------------
+----- There are some Instances of Non-Country Locations in the Data, which can be found by looking for cases with NULL in Continent:
+--SELECT DISTINCT(location)
+--FROM CovidDeaths
+--WHERE Continent IS NULL
+
+--- Besides the different continents and a world and international instance, 
+--- there are also Income Regions listed. Let's create a table Grouped by those Income locations:
+
+--DROP TABLE IF EXISTS #IncomeClass
+--CREATE TABLE #IncomeClasses (Classes NVARCHAR(50),
+--							 Date DATE,
+--							 Population BIGINT,
+--							 Total_Cases FLOAT,
+--							 Total_Deaths FLOAT,
+--							 New_Cases FLOAT,
+--							 New_Deaths FLOAT)
+--INSERT INTO #IncomeClasses
+--SELECT location, date, population, total_cases, total_deaths, new_cases, new_deaths
+--FROM CovidDeaths
+--WHERE location like '%income'
+
+--Overview--
+--SELECT *
+--FROM #IncomeClasses
+
+--Looking at Total Deaths, Total Cases and Death Cases Ratio in the different Income Regions--
+--SELECT Classes, MAX(Total_Cases) Total_Cases, MAX(Total_Deaths) Total_Deaths, FORMAT(ROUND(MAX(Total_Deaths)/ MAX(Total_cases), 4),'P') Ratio
+--FROM #IncomeClasses
+--GROUP BY Classes
+--ORDER BY MAX(Total_Deaths)/ MAX(Total_cases) DESC
+---- Low Income Countries have the highest Ratio, followed by upper middle, lower middle and High Income.
+---- According to the table there way more Total cases in High Income Countries than in Low income countries. However, this may be due to worse testing infrastructure
